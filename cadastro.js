56d4cf5ea8e6ab58ffc03d2e0dce697d6fbddcf7
Yasmyn <yasmyyynn@gmail.com>
2026-03-22 16:44:50 -0300

    Update and rename cuidados.js to cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,5 @@
 document.getElementById('formCadastro').addEventListener('submit', function(event) {
-    // Impede o formulário de recarregar a página imediatamente
+    
     event.preventDefault();
 
     const nome = document.getElementById('nome').value;
@@ -7,16 +7,15 @@ document.getElementById('formCadastro').addEventListener('submit', function(even
     const senha = document.getElementById('senha').value;
     const confirmarSenha = document.getElementById('confirmarSenha').value;
     const erroMsg = document.getElementById('mensagemErro');
-
-    // Validação de senha
+    
     if (senha !== confirmarSenha) {
         erroMsg.style.display = 'block';
-        return; // Para a execução aqui
+        return; 
     } else {
         erroMsg.style.display = 'none';
     }
 
-    // Se chegar aqui, está tudo certo
+   
     console.log("Dados capturados:", { nome, email, senha });
     
     alert(`Olá ${nome}! Seu cadastro no Gotas de Vida foi simulado com sucesso.`);
