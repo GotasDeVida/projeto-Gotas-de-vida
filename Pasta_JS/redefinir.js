57d9a3bf03aa289f97277d9772c257bf0aac0f16
Yasmyn <yasmyyynn@gmail.com>
2026-03-31 21:40:35 -0300

    Arrumação.em.pastas

--- Pasta_JS/redefinir.js
+++ Pasta_JS/redefinir.js
@@ -7,19 +7,16 @@ form.addEventListener("submit", function (e) {
 
     const usuario = JSON.parse(localStorage.getItem("usuario"));
 
-    // verifica se existe usuário
     if (!usuario) {
         alert("Nenhum usuário cadastrado!");
         return;
     }
 
-    // verifica se o email bate
     if (emailDigitado !== usuario.email) {
         alert("Email não encontrado!");
         return;
     }
 
-    // simulação de redefinição
     const novaSenha = prompt("Digite sua nova senha:");
 
     if (!novaSenha || novaSenha.length < 4) {
@@ -27,13 +24,11 @@ form.addEventListener("submit", function (e) {
         return;
     }
 
-    // atualiza senha
     usuario.senha = novaSenha;
 
     localStorage.setItem("usuario", JSON.stringify(usuario));
 
     alert("Senha redefinida com sucesso!");
 
-    // redireciona pro login
     window.location.href = "entrar.html";
 });
